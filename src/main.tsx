12d95a8de9e086cce14d4a738375461dbd3fd744
JC204 <jonesacolin2@gmail.com>
2025-05-04 13:22:44 -0700

    Update main.tsx with service worker handling and root rendering

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 import './index.css';
 
 // Unregister any existing service workers (prevents caching issues on Netlify)
